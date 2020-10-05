caddf07a529062ccc684e3a105af5c9bd2f214e3
Aqudi <gjdigj145@gmail.com>
2020-10-05 16:58:57 +0900

    Test html2canvas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+declare let html2canvas: any;
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,34 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'pdf-generator';
+  capturedImage;
+
+  clickme() {
+    html2canvas(document.querySelector("#capture")).then(canvas => {
+
+      debugger;
+
+      /// document.body.appendChild(canvas);
+      this.capturedImage = canvas.toDataURL();
+      console.log("canvas.toDataURL() -->" + this.capturedImage);
+      // this will contain something like (note the ellipses for brevity), console.log cuts it off 
+      // "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAa0AAAB3CAYAAACwhB/KAAAXr0lEQVR4Xu2dCdiNZf7HP/ZQkpQtaUxDjYYoTSYlURMhGlmKa..."
+
+
+      canvas.toBlob(function (blob) {
+
+        //  just pass blob to something expecting a blob
+        // somfunc(blob);
+
+        // Same as canvas.toDataURL(), just longer way to do it.
+        var reader = new FileReader();
+        debugger;
+        reader.readAsDataURL(blob);
+        reader.onloadend = function () {
+          let base64data = reader.result;
+          console.log("Base64--> " + base64data);
+        }
+      });
+    });
+  }
 }
